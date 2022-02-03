6dda26a2e7bf626c374a2d128810fef288e6416c
Tanz_ <66496612+Tanz17@users.noreply.github.com>
2022-02-03 21:47:26 +0300

    Update Events.java

--- Events.java
+++ Events.java
@@ -3,11 +3,23 @@ package ru.tanz.tblacknick;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Events implements Listener {
     TBlackNick plugin = TBlackNick.plugin();
     public static void setHealth(Player p, double hp){
         p.setMaxHealth(hp);
+    }
+    public void onDead(PlayerRespawnEvent e ){
+        if(plugin.uuid.contains(e.getPlayer().getUniqueId())){
+            for (String s : plugin.getConfig().getStringList("effect-on-give")) {
+                PotionEffectType type = PotionEffectType.getByName(s.substring(0, s.indexOf(";")));
+                e.getPlayer().addPotionEffect(new PotionEffect(type, 99999, Integer.valueOf(s.substring(s.lastIndexOf(";") + 1))));
+            }
+
+        }
     }
 }
 
